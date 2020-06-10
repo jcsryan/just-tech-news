e78eaf62c56a736aaf47f744f27ac958d4647195
JCSR <johnconnorsryan@gmail.com>
2020-06-10 14:45:41 -0400

    goddamn untracked bullshit

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 const router = require('express').Router();
+const apiRoutes = require('.');
 
-const apiRoutes = require('./api');
+
+const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
+
+
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 router.use('/api', apiRoutes);
 
